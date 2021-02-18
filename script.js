5d9a3a2e09f9d682fa905bcbecfff774960a46e9
guypystin <51515987+guypystin@users.noreply.github.com>
2021-02-18 16:59:26 +0300

    Значения инпутов записываются в таблицу

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-let arr = [];
-
-let first = function () {
-  let a = document.querySelector(".form-name").value;
-  return a;
-};
-let two = function () {
-  let a = document.querySelector(".form-price").value;
-  return a;
-};
-let three = function () {
-  let a = document.querySelector(".form-amount").value;
-  return a;
-};
-
-/*клеим строку из полученных выше значений инпутов*/
-let get_string = function () {
-  return first() + "," + two() + "," + three();
-};
-/*разбиваем строку на массив и записываем значения в arr*/
-let get_arr = function () {
-  return (arr = get_string().split(","));
-};
+let create_table = function (row, column) {
+    /* Обьявление всех необходимых переменных */
+    let prod_name = document.querySelector(".form-name");
+    let prod_price = document.querySelector(".form-price");
+    let prod_amount = document.querySelector(".form-amount");
+    let prod_price_sum = parseInt(prod_price.value) * parseInt(prod_amount.value);
+    var table = document.querySelector(".table");
+    var new_row = table.insertRow(-1);
+   
+    /* Ввод данных в таблицу */
+    function insert_rows() {
+     new_row.insertCell(-1).innerHTML = prod_name.value;
+     new_row.insertCell(-1).innerHTML = prod_price.value;
+     new_row.insertCell(-1).innerHTML = prod_amount.value;
+     new_row.insertCell(-1).innerHTML = prod_price_sum;
+    }
+   
+    /* проверка значений инпутов */
+    function check_value() {
+     if (
+      prod_name.value == "" ||
+      prod_price.value == "" ||
+      prod_amount.value == ""
+     ) {
+      alert("пожалуйста, заполни все поля!");
+     }
+     if (prod_price_sum > 0) {
+      insert_rows();
+     }
+    }
+    check_value();
+    /* отчистка инпутов после нажатия */
+    function clean() {
+     prod_name.value = "";
+     prod_price.value = "";
+     prod_amount.value = "";
+     prod_price_sum.value = "";
+    }
+    clean();
+   };
+   
